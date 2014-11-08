0d0919babc8f06f31bb7c7556300be21d3028e23
lautaroa14 <lautaroa14@gmail.com>
2014-11-08 00:17:39 -0200

    Arreglando un poco las vistas

--- app/src/main/java/com/pruebaclass1/camilo/StackOverflow/ResultadoActivity.java
+++ app/src/main/java/com/pruebaclass1/camilo/StackOverflow/ResultadoActivity.java
@@ -69,19 +69,36 @@ public class ResultadoActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.resultado, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+
+            case R.id.action_nuevo:
+                Intent i = new Intent(getApplicationContext(), NuevoActivity.class);
+                startActivity(i);
+                return true;
+
+            case R.id.action_tutorial:
+                Intent i2 = new Intent(getApplicationContext(), TutorialActivity.class);
+                startActivity(i2);
+                return true;
+
+            case R.id.action_ayuda:
+                Intent i3 = new Intent(getApplicationContext(), AyudaActivity.class);
+                startActivity(i3);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
         }
-        return super.onOptionsItemSelected(item);
     }
 }
